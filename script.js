6e90d620227dccb2c7d6a511684fcb3e8cdcff57
Milana Kagan <milana.l890@gmail.com>
2021-02-04 16:30:10 +0200

    score and answer check was added

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const $goAnimation = document.getElementById('go-animation');
 const $startBtn = document.getElementById('start-button');
 const $questionNumber = document.getElementById('question-number');
 const $question = document.getElementById('question');
+const $yesBtn= document.getElementById('yes');
+const $noBtn= document.getElementById('no');
+const $score= document.getElementById('score');
 
 //paramerers
 let $color = null;
@@ -16,8 +19,9 @@ let $math = null;
 
 let goIndex = 1;
 let counter = 0;
-let score = 0;
 let noTime = null;
+let score = 0;
+let result = null;
 
 let colorWord = null;
 let colorActual = ["orange", "yellow", "red", "brown", "green", "white", "blue", "purple", "black", "pink"];
@@ -32,13 +36,8 @@ let grammarResults = null;
 
 
 //functions
-const init = () => {
-    $startPage.style.display = 'flex';
-    $errorMassage.style.visibility = "hidden";
-    $gamePage.style.display = 'none';
-    $restartPage.style.display = 'none';
-    $goAnimation.style.display = 'none';
 
+const initQuestiosnData = () =>{
     colorWord = ["orange", "yellow", "red", "brown", "green", "white", "blue", "purple", "black", "pink"];
 
     mathQuestions = ["5 > 3","3 = 8","6 >= 5","1 < 0","0 = 0","2 < 7","8 > 3","9 = 6","2 = 2","4 >= 4"];
@@ -46,6 +45,16 @@ const init = () => {
 
     grammarQuestions = ["mother","fater","tabel","label","ecqual","canyon","short","langwhitch","karate","weazer"];
     grammarResults = [true,false,false,true,false,false,true,false,true,false];
+}
+
+const init = () => {
+    $startPage.style.display = 'flex';
+    $errorMassage.style.visibility = "hidden";
+    $gamePage.style.display = 'none';
+    $restartPage.style.display = 'none';
+    $goAnimation.style.display = 'none';
+   
+    initQuestiosnData();  
 }
 
 
@@ -103,24 +112,32 @@ const gameStart = () => {
         counter++;
         $questionNumber.innerHTML = (counter) + "/10";
         choiseIndex = Math.floor(Math.random() * choises.length);
-
+        
+        let question =null;
+        question = chooseQuestion(choises[choiseIndex]);
         if(choises[choiseIndex] == "color"){
-            let question = chooseQuestion(choises[choiseIndex]);
             $question.innerHTML = question[0];
             $question.style.color = question[1];
+            result = question[2];
         }
         else{
 
             $question.style.color = "#e6cf34";
-            $question.innerHTML = chooseQuestion(choises[choiseIndex])[0];
+            $question.innerHTML = question[0];
+            result = question[1];
         }
-        noTime = setTimeout(gameStart,2000);
+        
+        noTime = setTimeout(gameStart,5000);
     }
     else{
         clearTimeout(noTime);
+        noTime = null;
         counter = 0;
+        result = null;
         $gamePage.style.display = 'none';
         $restartPage.style.display = 'block';
+        $score.innerHTML = score;
+        score = 0;
     }
 
 }
@@ -165,7 +182,31 @@ const startPressed = () => {
        
 }
 
+const yesCheck = () => {
+
+    if (result == true)
+    {
+        score++;
+    }
+    clearTimeout(noTime);
+    gameStart();
+
+}
+
+
+const noCheck = () => {
+
+    if (result == false)
+    {
+        score++;
+    }
+    clearTimeout(noTime);
+    gameStart();
+}
+
 init();
 
 //listeners
 $startBtn.addEventListener('click',startPressed);
+$yesBtn.addEventListener('click',yesCheck);
+$noBtn.addEventListener('click',noCheck);
